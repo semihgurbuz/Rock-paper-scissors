922f556ec4fae599e35fdf46dc4d07bd70c9c99f
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-06 15:31:45 +0300

    made the game stop after a player reaches 5 points

--- script.js
+++ script.js
@@ -27,39 +27,92 @@ const playRound = (playerSelection, computerSelection) => {
   return result;
 };
 
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-  for (i = 0; i < 5; i++) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Enter your choice").toLowerCase();
-    let message = playRound(playerSelection, computerSelection);
-    if (message === "You lose !") {
-      computerScore++;
-      console.log(message);
-    } else if (message === "You win !") {
-      playerScore++;
-      console.log(message);
-    } else if (
-      message ===
-      "Invalid choice. You lose ! Please enter 'rock','paper' or 'scissors'."
-    ) {
-      alert(message);
-      computerScore++;
-    } else {
-      console.log(message);
-    }
-  }
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", () => {
+  getMessage();
+});
+
+const paper = document.querySelector(".paper");
+paper.addEventListener("click", () => {
+  getMessage();
+});
+
+const scissors = document.querySelector(".scissors");
+scissors.addEventListener("click", () => {
+  getMessage();
+});
+
+const scoreKeeper = () => {
   let score;
-  if (playerScore > computerScore) {
-    score = `Final score is ${playerScore}-${computerScore}, You Win !`;
-  } else if (playerScore < computerScore) {
-    score = `Final score is ${playerScore}-${computerScore}, You Lose !`;
-  } else {
-    score = `Final score is ${playerScore}-${computerScore}, Draw!`;
+  if (playerScore === 5) {
+    score = `Final score is ${playerScore}-${computerScore}, You Won ! Please refresh the page to page to start again.`;
+    document.querySelector(".final-result").textContent = score;
+  } else if (computerScore === 5) {
+    score = `Final score is ${playerScore}-${computerScore}, You Lost ! Please refresh the page to page to start again.`;
+    document.querySelector(".final-result").textContent = score;
   }
-  return score;
 };
 
-alert(game());
+const getMessage = () => {
+  let gameResult = document.querySelector(".results");
+
+  let message = playRound("paper", getComputerChoice());
+
+  gameResult.textContent = message;
+
+  if (message === "You lose !" && computerScore < 5 && playerScore != 5) {
+    computerScore++;
+  } else if (message === "You win !" && playerScore < 5 && computerScore != 5) {
+    playerScore++;
+  } else {
+    console.log(message);
+  }
+
+  document.querySelector(
+    ".player-score"
+  ).textContent = `Your score: ${playerScore}`;
+  document.querySelector(
+    ".computer-score"
+  ).textContent = `Computer score ${computerScore}`;
+  scoreKeeper();
+};
+
+// const game = () => {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   for (i = 0; i < 5; i++) {
+//     let computerSelection = getComputerChoice();
+//     let playerSelection = prompt("Enter your choice").toLowerCase();
+//     let message = playRound(playerSelection, computerSelection);
+//     if (message === "You lose !") {
+//       computerScore++;
+//       console.log(message);
+//     } else if (message === "You win !") {
+//       playerScore++;
+//       console.log(message);
+//     } else if (
+//       message ===
+//       "Invalid choice. You lose ! Please enter 'rock','paper' or 'scissors'."
+//     ) {
+//       alert(message);
+//       computerScore++;
+//     } else {
+//       console.log(message);
+//     }
+//   }
+//   let score;
+//   if (playerScore > computerScore) {
+//     score = `Final score is ${playerScore}-${computerScore}, You Win !`;
+//   } else if (playerScore < computerScore) {
+//     score = `Final score is ${playerScore}-${computerScore}, You Lose !`;
+//   } else {
+//     score = `Final score is ${playerScore}-${computerScore}, Draw!`;
+//   }
+//   return score;
+// };
+
+// alert(game());
